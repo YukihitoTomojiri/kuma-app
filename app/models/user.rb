45cdf6ba72ae627ca6ff9faa2a8b77eda5970581
YukihitoTomojiri <i.can.do.it.0922@gmail.com>
2023-01-24 15:46:51 +0900

    Fixed so that validation can be displayed correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   belongs_to :section
   belongs_to :job
 
+  validates :name,             presence: true
+  validates :birthday,         presence: true
+  VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
+  validates :postal_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
   validates :section_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :job_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end

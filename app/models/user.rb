2537375858b75098f72105442b5d66ffba964e74
YukihitoTomojiri <i.can.do.it.0922@gmail.com>
2023-01-27 05:44:33 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,15 @@ class User < ApplicationRecord
   validates :postal_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
   validates :section_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :job_id, numericality: { other_than: 1, message: "can't be blank" } 
-
+  
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end

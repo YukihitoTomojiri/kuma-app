f05f7f765664018067705f8b7ad191b3a8fe5df2
YukihitoTomojiri <i.can.do.it.0922@gmail.com>
2023-01-29 06:40:44 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,16 +24,6 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  
-  include JpPrefecture
-  jp_prefecture :prefecture_code
-  
-  def prefecture_name
-    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
-  end
-  
-  def prefecture_name=(prefecture_name)
-    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
-  end
+
   
 end
